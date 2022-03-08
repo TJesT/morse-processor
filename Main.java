e03d6f7f35267296a8ac5f3f64d890dc95aba3a9
TJesT <alexeynospid@gmail.com>
2022-03-08 22:13:09 +0700

    mfp mock process multythreading

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
-import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String cmd = scanner.next();
-        ArrayList<String> arg = new ArrayList<>();
-        while (scanner.hasNext()) {
-            arg.add(scanner.next());
-        }
-
         MorseFileProcessor mfp = MorseFileProcessor.getInstance();
 
-        mfp.execute(cmd, arg);
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 10 && scanner.hasNextLine(); ++i){
+            String line = scanner.nextLine().trim();
+
+            List<String> arg = Stream.of(line.split(" "))
+                    .map(String::new)
+                    .collect(Collectors.toList());
+
+            String cmd = arg.get(0);
+            arg.remove(0);
+
+            mfp.execute(cmd, arg);
+        }
 
         scanner.close();
     }
--- MorseFileProcessor.java
+++ MorseFileProcessor.java
@@ -1,17 +1,20 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.List;
 
 public class MorseFileProcessor {
     private static MorseFileProcessor instance;
-    private HashMap<String, Method> commands;
-    private ArrayList<String> args;
+    private final HashMap<String, Method> commands;
+    private final HashMap<String, Object> arguments;
 
     private MorseFileProcessor() throws NoSuchMethodException {
         this.commands = new HashMap<>();
         this.commands.put("code", MorseFileProcessor.class.getDeclaredMethod("encode"));
         this.commands.put("decode", MorseFileProcessor.class.getDeclaredMethod("decode"));
+
+        this.arguments = new HashMap<>();
+        this.arguments.put("file-name", null);
     }
 
     public static MorseFileProcessor getInstance() {
@@ -25,22 +28,78 @@ public class MorseFileProcessor {
         return instance;
     }
 
-    public void execute(String cmd, ArrayList<String> args) {
-        this.args = args;
+    public void execute(String cmd, List<String> args) {
+        while(!args.isEmpty()) {
+            String flag = args.get(0), argument = args.get(1);
+            args.remove(0); args.remove(0);
+            switch (flag) {
+                case "-f": {
+                    this.arguments.put("file-name", argument);
+                    break;
+                }
+                default: {
+                    //TODO: log invalid argument or throw exception
+                    break;
+                }
+            }
+        }
         try {
-            this.commands.get(cmd).invoke(null);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
+            this.commands.get(cmd).invoke(getInstance());
+
+        } catch (NullPointerException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
 
-    private static void encode() {
-        System.out.println("encode invoked");
+    private void encode() {
+        Encoder encoder = new Encoder(this.arguments.get("file-name").toString());
+        encoder.start();
     }
 
-    private static void decode() {
-        System.out.println("decode invoked");
+    private void decode() {
+        Decoder decoder = new Decoder(this.arguments.get("file-name").toString());
+        decoder.start();
+    }
+}
+
+class Encoder extends Thread {
+    String file_name;
+    public Encoder(String file_name) {
+        this.file_name = file_name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(
+                    "Encode of " + this.file_name
+                    + " invoked ! Thread "
+                    + Thread.currentThread().getId()
+                    + " is running .");
+        }
+        catch (Exception e) {
+            System.out.printf("Exception caught !");
+        }
+    }
+}
+
+class Decoder extends Thread {
+    String file_name;
+    public Decoder(String file_name) {
+        this.file_name = file_name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(
+                    "Decode of " + this.file_name
+                    + " invoked ! Thread "
+                    + Thread.currentThread().getId()
+                    + " is running .");
+        }
+        catch (Exception e) {
+            System.out.printf("Exception caught !");
+        }
     }
 }
